d7d21253ed96bbd1b0969ca59be9c6d673365771
sanjay kumar <sanjayjnu22@gmail.com>
2021-05-07 17:57:09 +0530

    ts config change

--- api-doc-server.js
+++ api-doc-server.js
@@ -2,4 +2,4 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'doc')));
-app.listen(2553, () => { console.log('api doc is running') });
+app.listen(2555, () => { console.log('api doc is running') });
